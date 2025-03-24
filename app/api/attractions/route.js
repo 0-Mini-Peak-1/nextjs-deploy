79d03d71389f2443f6f370e7956684fbf8ea4cd7
0-Mini-Peak-1 <xamiprogo@gmail.com>
2025-03-24 13:59:11 +0700

    after adding CRUD but not yet finished

--- app/api/attractions/route.js
+++ app/api/attractions/route.js
@@ -8,9 +8,22 @@ export async function GET(request) {
     const [rows, fields] = await db.query(
       'SELECT * FROM attractions'
     )
-    return NextResponse.json(rows,{status:200})
-  } catch (error){
-    return NextResponse.json({error:"Failed to fetch"}, {status: 500})
+    return NextResponse.json(rows, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to fetch" }, { status: 500 })
   }
 }
+
+export async function POST(request) {
+  try {
+    const { name, detail, coverimage, latitude, longitude } = await request.json();
+    const [result] = await db.query(
+      'INSERT INTO attractions (name, detail,coverimage, latitude, longitude) VALUES (?,?,?,?,?)', [name, detail, coverimage, latitude, longitude]
+    );
+    return NextResponse.json({ id: result.insertId, name, detail, coverimage, latitude, longitude }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to create" }, { status: 500 })
+  }
+}
+
 
